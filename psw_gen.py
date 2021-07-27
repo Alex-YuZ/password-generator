a79e112aaaf541876e503c21cf92faecd9b6c027
Alex-Yu <alexyuzhu@gmail.com>
2021-07-28 01:37:39 +0800

    feat: Add two ways to generate passwords
    - Default generation
    - Customized generation

--- psw_gen.py
+++ psw_gen.py
@@ -1,27 +1,55 @@
-#%%
 import string
 import random
 
 class Password():
     
-    def __init__(self, elements=None):
+    def __init__(self, elements=None, length=None):
+        """[summary]
+
+        Args:
+            elements (str, optional): elements used to generate password. Defaults to None.
+
+            length (int, optional): length of password. Defaults to None.
+            
+        Example_1:
+            psw1 = Password()
+            psw1.generate()
+            
+        Example_2:
+            psw2 = Password("ABCXYZabc0123@#$", 6)
+            psw1.generate()
+        """
         
-        # memebers in password
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        nums = "".join(list(map(str, range(10))))
-        symbles = "[]{}()*;/,_-@"
+        if elements is None:
+            # memebers in password
+            lower = string.ascii_lowercase
+            upper = string.ascii_uppercase
+            nums = "".join(list(map(str, range(10))))
+            symbles = "[]{}()*;/,_-@"
 
-        # join all memembers above together
-        self.cmb = lower + upper + nums + symbles
+            # join all memembers above together
+            self.cmb = lower + upper + nums + symbles
+            
+        else:
+            self.cmb = elements
 
-        # length of the password
-        self.length = 16
+        if length is None:
+            # length of the password
+            self.length = 16
+   
+        else:
+            self.length = length
 
     def generate(self):
-        # generate random password
-        psw = "".join(random.sample(self.cmb, self.length))
-        return psw
+        """generate the password"""
+        
+        self.psw = "".join(random.sample(self.cmb, self.length))
+        print(self.psw)
+        # return self.psw
+    
+    def __str__(self):
+        return "This is an instance of Password class"
+    
+    def __repr__(self):
+        return "CLASS OBJECT: Password(elements, length)".format()
 
-psw1 = Password()
-print(psw1.generate())
