8fd7f47976848645b6eb6e63ff1a815d214b63ec
Alex-Yu <alexyuzhu@gmail.com>
2021-07-28 01:18:56 +0800

    refactor: refactor codes using a classs object

--- psw_gen.py
+++ psw_gen.py
@@ -2,21 +2,26 @@
 import string
 import random
 
+class Password():
+    
+    def __init__(self, elements=None):
+        
+        # memebers in password
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        nums = "".join(list(map(str, range(10))))
+        symbles = "[]{}()*;/,_-@"
 
-# memebers in password
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-nums = "".join(list(map(str, range(10))))
-symbles = "[]{}()*;/,_-@"
+        # join all memembers above together
+        self.cmb = lower + upper + nums + symbles
 
-# join all memembers above together
-cmb = lower + upper + nums + symbles
+        # length of the password
+        self.length = 16
 
-# length of the password
-length = 16
+    def generate(self):
+        # generate random password
+        psw = "".join(random.sample(self.cmb, self.length))
+        return psw
 
-# generate random password
-psw = "".join(random.sample(cmb, length))
-
-print(psw)
-
+psw1 = Password()
+print(psw1.generate())
